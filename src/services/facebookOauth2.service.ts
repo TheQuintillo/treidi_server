e18e5fac36263a3ac7010c1ec6a008f60e97a3dd
TheQuintillo <thequintillo@gmail.com>
2023-05-30 13:22:50 +0200

    feat: Added settings service passport strategy Google and Facebook

--- src/services/facebookOauth2.service.ts
+++ src/services/facebookOauth2.service.ts
@@ -1,20 +1,48 @@
 import passport from "passport";
+import { UserFacebookTreidi } from "../entities/Users/UserFacebook.entities";
+import { UserFacebookPrisma } from "../models/Users/UserFacebook.model";
+
 const FacebookStrategy = require("passport-facebook");
 
+const user = new UserFacebookPrisma();
 passport.use(
   new FacebookStrategy(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
       callbackURL: process.env.FACEBOOK_URL_CALLBACK,
+      profileFields: ["id", "emails", "name"],
     },
-    function (accessToken, refreshToken, profile, cb) {
-      //ADDED new
-      // When models prisma UserFacebook will be created settings this section with new User
-      /*
-      User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });*/
+    async function (
+      accessToken: any,
+      refreshToken: any,
+      profile: any,
+      cb: any
+    ) {
+      const findUser = await user.findUserById(profile.id);
+
+      if (findUser) {
+        return cb(null, findUser);
+      } else {
+        const User = new UserFacebookTreidi(
+          profile.id,
+          profile.name.givenName + " " + profile.name.familyName,
+          profile.email,
+          accessToken
+        );
+        const createUser = await user.createUser(User);
+
+        return cb(null, createUser);
+      }
     }
   )
 );
+
+passport.serializeUser(function (user, done) {
+  console.log("SERIALIZING", user);
+  done(null, user);
+});
+passport.deserializeUser(async (user: UserFacebookTreidi | null, done) => {
+  console.log("Deserializing", user);
+  done(null, user);
+});
--- src/services/googleOAuth2.service.ts
+++ src/services/googleOAuth2.service.ts
@@ -1,5 +1,10 @@
 import passport from "passport";
+import { UserGoogleTreidi } from "../entities/Users/UserGoogle.entities";
+import { UserGooglePrisma } from "../models/Users/UserGoogle.model";
+
 var GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const user = new UserGooglePrisma();
 
 passport.use(
   new GoogleStrategy(
@@ -8,11 +13,37 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_URL_CALLBACK,
     },
-    function (accessToken, refreshToken, profile, cb) {
-      // When models prisma UserGoogle will be created settings this section with new User
-      /*User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });*/
+    async function (
+      accessToken: any,
+      refreshToken: any,
+      profile: any,
+      cb: any
+    ) {
+      const findUser = await user.findUser(profile.emails[0].value);
+
+      if (findUser) {
+        return cb(null, findUser);
+      } else {
+        const User = new UserGoogleTreidi(
+          profile.id,
+          profile.displayName,
+          profile.emails[0].value,
+          profile.photos[0].value,
+          accessToken
+        );
+        const createUser = await user.createUser(User);
+
+        return cb(null, createUser);
+      }
     }
   )
 );
+
+passport.serializeUser((user, done) => {
+  console.log("Serializing", user);
+  done(null, user);
+});
+passport.deserializeUser(async (user: UserGoogleTreidi | null, done) => {
+  console.log("Deserializing", user);
+  done(null, user);
+});
