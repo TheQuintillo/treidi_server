e5cfd460d99b12bbd39041ceec769cd13f5d8b04
TheQuintillo <thequintillo@gmail.com>
2023-05-30 13:23:28 +0200

    feat: Add router index.ts and initialize passport

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,24 @@ import cors from "cors";
 import express from "express";
 import { PORT } from "./config/env";
 import indexRouter from "./routes/index.route";
+import authGoogle from "./routes/auth/authGoogle.routes";
+import authFacebook from "./routes/auth/authFacebook.routes";
+import registerRouter from "./routes/register/register.route";
+import passport from "passport";
 import session from "./middlewares/session.middleware";
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use("/auth-google", authGoogle);
+app.use("/auth-facebook", authFacebook);
+app.use("/register", registerRouter);
 app.use("/", indexRouter);
 
 app.listen(PORT, () => {
